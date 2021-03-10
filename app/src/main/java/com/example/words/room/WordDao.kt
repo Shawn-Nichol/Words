10c0ba51c0ea7790a08ec966fdab8a49e77c15f9
Shawn Nichol <7067shawnnichol@gmail.com>
2021-03-09 18:46:26 -0700

    Word DAO added

---
+++ app/src/main/java/com/example/words/room/WordDao.kt
@@ -0,0 +1,19 @@
+package com.example.words.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface WordDao {
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+
+    @Delete
+    suspend fun deleteWord(word: Word)
+}
