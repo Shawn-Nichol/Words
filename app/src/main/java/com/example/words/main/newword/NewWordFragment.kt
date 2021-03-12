ef824b1264eacdd93940b8f01755f96e4080e13f
Shawn Nichol <7067shawnnichol@gmail.com>
2021-03-11 23:32:18 -0700

    NewWordActivity.kt removed from app. NewWordFragment.kt replaced it.

--- app/src/main/java/com/example/words/main/newword/NewWordFragment.kt
+++ app/src/main/java/com/example/words/main/newword/NewWordFragment.kt
@@ -1,22 +1,17 @@
 package com.example.words.main.newword
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.words.MyApplication
 import com.example.words.R
-import com.example.words.databinding.ActivityNewWordBinding
 import com.example.words.databinding.FragmentNewWordBinding
 import com.example.words.main.MainViewModel
-import com.example.words.newword.NewWordActivity
 import com.example.words.room.Word
 import javax.inject.Inject
 
