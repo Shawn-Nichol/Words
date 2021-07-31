850085790530e943b0b3d91377a1402a3ff82f5a
Shawn Nichol <7067shawnnichol@gmail.com>
2021-07-31 16:09:37 -0600

    MainActivity.kt added navigation drawer to app, Items in navigation drawer aren't clickable yet.

--- app/src/androidTest/java/com/example/words/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/words/main/MainActivityTest.kt
@@ -1,12 +1,16 @@
 package com.example.words.main
 
+import android.app.Activity
 import android.content.Context
+import androidx.appcompat.widget.Toolbar
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -104,5 +108,66 @@ class MainActivityTest {
 
         Assert.assertEquals(MODE_DAY, currentMode)
 
+    }
+
+    @Test
+    fun navDrawer_slideOpen() {
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+            .check(ViewAssertions.matches(DrawerMatchers.isClosed()))
+            .perform(DrawerActions.open())
+    }
+
+    @Test
+    fun navDrawer_click_HomeIcon() {
+        Espresso.onView(ViewMatchers.withContentDescription(scenario.getToolbarNavigationContentDescription()))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+            .check(ViewAssertions.matches(DrawerMatchers.isOpen()))
+
+    }
+
+    @Test
+    fun navDrawer_slideOpen_Fragment1_click() {
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+            .check(ViewAssertions.matches(DrawerMatchers.isClosed()))
+            .perform(DrawerActions.open())
+
+        Espresso.onView(ViewMatchers.withText("First"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        Thread.sleep(2000)
+
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+            .check(ViewAssertions.matches(DrawerMatchers.isClosed()))
+    }
+
+    @Test
+    fun navDrawer_slideOpen_Fragment2_click() {
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+            .perform(DrawerActions.open())
+
+        Espresso.onView(ViewMatchers.withText("Second"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+            .check(ViewAssertions.matches(DrawerMatchers.isClosed()))
+
+    }
+
+
+
+
+    fun <T : Activity> ActivityScenario<T>.getToolbarNavigationContentDescription()
+            : String {
+        var description = ""
+        onActivity {
+            description =
+                it.findViewById<Toolbar>(R.id.toolbar).navigationContentDescription as String
+        }
+        return description
     }
 }
--- app/src/main/java/com/example/words/main/MainActivity.kt
+++ app/src/main/java/com/example/words/main/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.words.main
 
-import android.content.res.Configuration
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.*
 import com.example.words.MyApplication
 import com.example.words.R
 import com.example.words.databinding.ActivityMainBinding
 import com.example.words.data.room.WordDao
+import com.google.android.material.navigation.NavigationView
+
 import javax.inject.Inject
 
 var TAG = "MyTest"
@@ -33,6 +38,12 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private lateinit var appBarConfiguration:AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as MyApplication).appComponent.inject(this)
         supportFragmentManager.fragmentFactory = MainFragmentFactory(viewModel)
@@ -41,12 +52,12 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.binding = this
 
-
+        // Toolbar is the action bar.
         setSupportActionBar(findViewById(R.id.toolbar))
-        val navHostFragment: Fragment? =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        val navController = navHostFragment!!.findNavController()
-        NavigationUI.setupActionBarWithNavController(this, navController)
+
+
+        initNavigation()
+        initNavDrawerClickListener()
 
     }
 
@@ -62,7 +73,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp()
+        return navController.navigateUp(appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -92,4 +103,45 @@ class MainActivity : AppCompatActivity() {
     private fun changeThemeMode(mode: Int) {
         AppCompatDelegate.setDefaultNightMode(mode)
     }
+
+    private fun initNavigation() {
+
+        // Get FragmentManager for nav_host_fragment
+        val navHostFragment: Fragment? =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+
+        // Get NavController for NavHostFragment
+        navController = navHostFragment!!.findNavController()
+
+        drawerLayout = binding.drawerLayout
+
+        //Customize the UI of the Action bar.
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.dest_wordListFragment), drawerLayout)
+
+        // Setup the action bar for use with navController
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun initNavDrawerClickListener() {
+        val navView = findViewById<NavigationView>(R.id.nav_drawer_view)
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.nav_first_fragment -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    Toast.makeText(this, "Fragment one", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.nav_second_fragment -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    Toast.makeText(this, "Fragment two", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
+
 }
